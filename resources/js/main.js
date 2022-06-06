261cd535747923e0b423cf3e0e0bd1e10344e04c
GS Hani <hani_alazawi@hotmail.com>
2022-06-06 15:09:28 +0300

    add dark-mode button

---
+++ resources/js/main.js
@@ -0,0 +1,6 @@
+function darkMode() {
+    document.body.classList.toggle("dark-mode");
+    document.getElementById('nav').classList.toggle("dark-mode");
+    document.getElementById('skills').classList.toggle("dark-image");
+    document.getElementById('contact').classList.toggle("dark-image");
+ }
